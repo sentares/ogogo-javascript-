0e8f43d9bd7ec94112535a71c61b52e63703270f
sentares <aydararzybekov2002@mail.ru>
2022-09-28 21:33:31 +0600

    my-class-work

---
+++ index.js
@@ -0,0 +1,188 @@
+// for (let i = 0; i < 5; i++) {
+//     let j = 0 
+//     console.log(j);
+//     j++
+//     console.log(j);
+// }
+
+
+// 1 задание 
+// let numbers = [1,2,3,4,5]
+
+// for (let key in numbers) {
+//     console.log(numbers[key]);
+// }
+
+
+
+// 2 задание 
+
+// let sum = [2,3,4,5]
+// for (let key in sum) {    
+//     console.log(sum[key]);
+// }
+
+// let result = 1
+// let a = [2,3,4,5]
+
+// for (let i=0; i < a.length; i++){
+//     result = result * a[i]
+   
+// }
+// console.log(result);
+
+
+// 3 задание 
+
+// const obj = {
+// 	'Минск': 'Беларусь',
+// 	'Москва': 'Россия',
+// 	'Киев': 'Украина'
+// };
+// for (let key in obj) {
+//     console.log(key + ' - это ' + obj[key]);
+// }
+
+
+// 4 задание 
+
+// for (let i = 11; i < 34; i++) {
+//     console.log(i);
+// }
+
+
+// 5 задание 
+
+// const salary = {
+//     коля: '200',
+//     вася: '300',
+//     петя: '400'
+// }
+
+// for (let key in salary)
+//     console.log(key + ' - зарплата ' + salary[key] + ' долларов');
+
+
+// 6 задание 
+
+// for (let i = 1000; 50 < i; i++) {
+//     result = i /= 2
+//     console.log(result);
+// }
+
+
+
+// 7 задание 
+
+// const num = 10 
+
+// function takebul(ask) {
+//     if (num === 10) {
+//         console.log('верно');
+//     } else  {
+//         console.log('не верно');
+//     }
+// }
+// console.log(takebul(num));
+
+
+
+// 8 задание 
+
+// let min = prompt ('введите число от 0 до 59', '')
+
+// if (min >= 0 && min <=14){
+//     alert('в первую');
+// }
+// if (min >=15 && min <= 30){
+//     alert('во вторую');
+// }
+// if (min >=31 && min <= 45){
+//     alert('в третью');
+// }
+// if (min >=46 && min <= 59){
+//     alert('В четвертую');
+// }
+
+
+
+
+// 9 задание 
+
+// let lang = prompt ('введите либо ru либо en', '');
+// if (lang == 'ru'){
+//     let rlang = ['пн', 'вт','ср','чт','сб','вс'];
+//     alert (rlang)
+// } else if (lang == 'en') {
+//     let elang = ['mn', 'ts','wd','th','fr','sn'];
+//     alert (elang)
+// }
+    
+
+
+
+
+// 10-11-12 задания (я не стал писать новый код т.к суть одна и таже)
+
+// let zeros = prompt('введите числа (1 , 0 , -3)', '');
+
+// if (zeros == 0) {
+//   alert( 'верно' );
+// } else if (zeros > 0) {
+//   alert( 'неверно' );
+// } else if (zeros < 0) {
+//     alert( 'неверно') 
+// }
+
+
+
+// 14-15-16 задание (ситуация как и с предыдущими заданиями)
+
+// let zeros = prompt('введите числа (1 , 0 , -3)', '');
+
+// if (zeros => 0) {
+//   alert( 'верно' );
+// } else if (zeros > 0) {
+//   alert( 'неверно' );
+// } else if (zeros < 0) {
+//     alert( 'неверно') 
+// }
+
+// let zeros = prompt('введите числа (1 , 0 , -3)', '');
+
+// if (zeros !== 0) {
+//   alert( 'верно' );
+// } else if (zeros > 0) {
+//   alert( 'неверно' );
+// } else if (zeros < 0) {
+//     alert( 'неверно') 
+// }
+
+
+// 17 задание 
+
+// let quetion = prompt ('напишите слова (test , тест , 3)', '' );
+
+// if (quetion === 'test') {
+//     alert('верно')
+// } else if (quetion !== 'test') {
+//     alert('неверно')
+// }
+
+
+// 18 задание  я попробовал поставить === но у меня выдовало ошибку, я так и не понял почему
+
+// let blabla = prompt ("напишите цифру ( '1', 1 , 3)", '' );
+
+// if (blabla == 1 ) {
+//     alert('верно')
+// } else if (blabla != 1) {
+//     alert('неверно')
+// }
+
+
+
+
+
+
+
