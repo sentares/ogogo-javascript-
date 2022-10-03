cf8c2db5894c71f55cc3fd335001ddeeced3e332
sentares <aydararzybekov2002@mail.ru>
2022-10-03 15:48:03 +0600

    first commit

---
+++ scripy.js
@@ -0,0 +1,81 @@
+// let blockHtml = document.querySelector('block')
+// let htmlP = document.createElement('p')
+// htmlP.innerText = 'hello'
+// htmlP.style.color = 'white'
+
+// console.log(blockHtml);
+
+let body = document.querySelector ('body')
+body.style.background = 'grey'
+body.style.maxWidth = '1440'
+
+let body1 =  document.querySelector('.main')
+body1.style.alightItems = 'center'
+body1.style.justifyContent = 'center'
+body1.style.marginLeft = '215px'
+body1.style.marginTop = '215px'
+body1.style.width = '425px'
+body1.style.height = '425px'
+body1.style.background = 'white'
+body1.style.position = 'relative'
+
+
+let text = document.querySelector('.text')
+text.style.display = 'flex'
+text.style.justifyContent = 'center'
+text.style.paddingTop = '50px'
+text.style.fontSize = '16px'
+
+
+let data1 = document.querySelector('.data')
+data1.style.marginTop = '30px'
+data1.style.display = 'flex'
+data1.style.justifyContent = 'center'
+data1.style.coloe = 'black'
+
+let email = document.querySelector('.email')
+email.style.color = 'grey'
+email.style.border = '0'
+
+
+let data2 = document.querySelector('.data2')
+data2.style.marginTop = '30px'
+data2.style.display = 'flex'
+data2.style.justifyContent = 'center'
+data2.style.coloe = 'black'
+
+
+let password = document.querySelector('.password')
+password.style.color = 'grey'
+password.style.border = '0'
+
+
+let b = document.querySelector('.b')
+b.style.marginTop = '30px'
+b.style.display = 'flex'
+b.style.justifyContent = 'center'
+
+
+let button = document.querySelector('button')
+button.style.paddingLeft = '70px'
+button.style.paddingRight = '70px'
+button.style.paddingTop = '10px'
+button.style.paddingBottom = '10px'
+// button.style.padding = '70px, 20px, 70px, 20px'
+button.style.fontSize = '25px'
+button.style.background = '#0F303F'
+button.style.color = 'white'
+button.style.cursor = 'pointer'
+
+let sslk = document.querySelector('.sslk')
+sslk.style.marginTop = '40px'
+sslk.style.display = 'flex'
+sslk.style.justifyContent = 'center'
+
+let a = document.querySelector('a')
+a.style.color = '#E64926'
+a.style.textDecoration = 'none'
+
+let a1 = document.querySelector('.a1')
+a1.style.color = '#E64926'
+a1.style.textDecoration = 'none'
